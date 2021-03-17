8546f49ff8ffee52e44c1f6f3481c52228056ebb
José Manuel Cornelio <batcornelio@gmail.com>
2021-03-16 20:09:48 -0400

    Registro de usuarios en la base de datos

--- basedatos.sql
+++ basedatos.sql
@@ -2,17 +2,18 @@ CREATE DATABASE IF NOT EXISTS master_python;
 USE master_python;
 
 CREATE TABLE usuarios(
-    id INT(25) NOT NULL,
+    id INT(25) NOT NULL AUTO_INCREMENT,
     nombre VARCHAR(100),
     apellidos VARCHAR(255),
     email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
     fecha DATE NOT NULL,
     CONSTRAINT pk_usuarios PRIMARY KEY(id),
     CONSTRAINT uq_email UNIQUE(email)
 ) ENGINE = InnoDb;
 
 CREATE TABLE notas(
-    id INT(25) NOT NULL,
+    id INT(25) NOT NULL AUTO_INCREMENT,
     usuario_id INT(25) NOT NULL,
     titulo VARCHAR(255) NOT NULL,
     descripcion MEDIUMTEXT,
--- usuarios/acciones.py
+++ usuarios/acciones.py
@@ -1,3 +1,5 @@
+import usuarios.usuario as modelo
+
 class Acciones:
 
     def login(self):
@@ -12,3 +14,11 @@ class Acciones:
         apellidos = input("¿Cual es tu apellido?: ")
         email = input("Introduce tu e-mail: ")
         password = input("Introduce tu contraseña: ")
+
+        usuario = modelo.Usuario(nombre, apellidos, email, password)
+        registro = usuario.registrar()
+
+        if registro[0] >= 1:
+            print(f"Perfecto {registro[1].nombre}. Te has registrado correctamente!!!")
+        else:
+            print("Lo siento, algo ha malido sal")
